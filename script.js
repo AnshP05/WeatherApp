a58876e21af1e5b07007b5888bf72d92a10e6c50
AnshP05 <23parasharansh@gmaill.com>
2025-07-23 14:28:30 -0700

    Added a units button for user functionality. Had some issues where it would work but wouldn't work on the defaulted city so added a current city variable so that when the units swap button is clicked, it loads the data for the current city in whatever units the user wants

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+let imperialUnits = true
+let currentCity = "Los Angeles"
 let weather =  {
     "apiKey": "ad24e848c0460261719422f8e020a356",
     fetchWeather : function (city) {
+        currentCity = city
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q="
              + city
-             + "&units=imperial&appid="
+             + "&units="
+             + (imperialUnits ? "imperial" : "metric")
+             + "&appid="
              + this.apiKey
         )
             .then((response) => response.json())
@@ -19,16 +24,23 @@ let weather =  {
         document.querySelector(".city").innerText = "Weather in " + name
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png"
         document.querySelector(".description").innerText = description
-        document.querySelector(".temp").innerText = temp + "°F"
+        document.querySelector(".temp").innerText = temp + (imperialUnits ? " °F" : " °C")
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%"
-        document.querySelector(".wind").innerText = "Wind Speed: " + speed + "mph"
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + (imperialUnits ? " mph" : " kph")
         document.querySelector(".weather").classList.remove("loading")
     },
     search : function() {
         this.fetchWeather(document.querySelector(".search-bar").value)
     }
 }
-
+document.querySelector(".units-button").addEventListener("click", function() {
+    imperialUnits = !imperialUnits
+    const unitsText = this.childNodes[0]
+    unitsText.textContent = imperialUnits ? "Imperial" : "Metric"
+    const icon = this.querySelector("svg")
+    icon.classList.toggle("rotate")
+    weather.fetchWeather(currentCity)
+})
 document.querySelector(".search button").addEventListener( "click", function() {
     weather.search()
 })
