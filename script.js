2d5a6277c75ec88a6fb9828922c55a11e53b1ec6
AnshP05 <23parasharansh@gmaill.com>
2025-07-23 16:24:02 -0700

    Fixed issue where when using geolocation and then switching the units, it defauled to Los Angeles rather than converint the units of the current city as well as a small error of unit conversion. The API gives metric data for speed in m/s and I assumed the gave in km/hr so i just added a conversion step in the weather objects displayWeather function

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 let imperialUnits = true
 let currentCity = "Los Angeles"
+let usingCoords = false;
+let currentCoords = { lat : null, long : null}
+const options = {
+    method : 'GET',
+    headers : {
+        'X-RapidAPI-Key': ' 6cd95d140bmsh798798f899795cdp1da559jsn141f24f3cd78',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+}
 let weather =  {
     "apiKey": "ad24e848c0460261719422f8e020a356",
     fetchWeather : function (city) {
@@ -14,7 +23,7 @@ let weather =  {
         )
             .then((response) => response.json())
             .then((data) => this.displayWeather(data))
-
+        usingCoords = false;
     },
     displayWeather : function(data) {
         const { name } = data
@@ -26,20 +35,36 @@ let weather =  {
         document.querySelector(".description").innerText = description
         document.querySelector(".temp").innerText = temp + (imperialUnits ? " °F" : " °C")
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%"
-        document.querySelector(".wind").innerText = "Wind Speed: " + speed + (imperialUnits ? " mph" : " kph")
+        document.querySelector(".wind").innerText = "Wind Speed: " + (imperialUnits ? speed : (speed * 3.6).toFixed(1)) + (imperialUnits ? " mph" : " kph");
+
         document.querySelector(".weather").classList.remove("loading")
     },
     search : function() {
         this.fetchWeather(document.querySelector(".search-bar").value)
+    }, 
+    fetchWeatherByCoords : function(lat, lon) {
+        usingCoords = true;
+        currentCoords.lat = lat;
+        currentCoords.long = lon;
+        fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${imperialUnits ? "imperial" : "metric"}&appid=${this.apiKey}`
+    )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
     }
-}
+    }
+
 document.querySelector(".units-button").addEventListener("click", function() {
     imperialUnits = !imperialUnits
     const unitsText = this.childNodes[0]
     unitsText.textContent = imperialUnits ? "Imperial" : "Metric"
     const icon = this.querySelector("svg")
     icon.classList.toggle("rotate")
-    weather.fetchWeather(currentCity)
+    if(usingCoords) {
+        weather.fetchWeatherByCoords(currentCoords.lat, currentCoords.long)
+    } else {
+        weather.fetchWeather(currentCity)
+    } 
 })
 document.querySelector(".search button").addEventListener( "click", function() {
     weather.search()
@@ -51,4 +76,25 @@ document.querySelector(".search-bar").addEventListener('keyup', function(event) 
     }
 })
 
-weather.fetchWeather("Los Angeles")
+function fetchCities(query) {
+    fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${query}&limit=5`, options)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.data); // Array of city objects
+      // TODO: show suggestions in dropdown
+    })
+    .catch(error => console.error('Error fetching cities:', error));
+}
+
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition( (position)  => {
+        const lat = position.coords.latitude
+        const long = position.coords.longitude
+
+        weather.fetchWeatherByCoords(lat, long)
+    },
+    () => weather.fetchWeather("Los Angeles")
+    )
+} else {
+    weather.fetchWeather("Los Angeles")
+}
